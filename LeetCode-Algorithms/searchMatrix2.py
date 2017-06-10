5d4ef5f3818e754e4feaf7c8d3a762a10d52bc40
Ashish Katiyar <ashish4feb@users.noreply.github.com>
2017-06-10 02:24:29 -0400

    240. Search a 2D Matrix II

---
+++ LeetCode-Algorithms/searchMatrix2.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if len(matrix)==0 or len(matrix[0])==0:
+            return False
+        if target < matrix[0][0] or target > matrix[-1][-1]:
+            return False
+        i = 0
+        j = len(matrix[0])-1
+        while i<=len(matrix)-1 and j>=0:
+            if matrix[i][j]==target:
+                return True
+            elif matrix[i][j]>target:
+                j-=1
+            else:
+                i+=1
+        return False
